b00ac0d2ce0823e1472ea324f958de8f03e7a283
Brahyan Martinez <brahyanpro16@gmail.com>
2024-05-11 21:03:16 -0400

    Actualizar archivos relacionados con el scraping de profesores y comentarios en la UASD

--- maestros_materias.js
+++ maestros_materias.js
@@ -1,138 +1,120 @@
+import { chromium } from 'playwright';
 import allTeachers from './teachers.json' assert { type: 'json' };
 import allSubjects from './subjects.json' assert { type: 'json' };
-import { chromium } from 'playwright';
+import teacherData from './unique_matcheds.json' assert { type: 'json' };
 import fs from 'fs/promises';
 
 console.log('Inicio de la comparación de nombres...');
 console.time('Tiempo de ejecución');
 
-const POINTS_BY_SAME_SPOT = 2;
-const POINTS_BY_OCCURRENCE = 1;
-const cleanName = name => {
-  return name.replace(/\s+/g, ' ').trim(); // Simplifica espacios y remueve espacios extras
-};
-
-const getInitials = name => {
-  return name.split(' ').map(word => word[0]);
-};
-
-const nsemestreNames = listNuevoSemestre.map(item => cleanName(item.name));
-const oursNames = listOurNames.map(item => ({ ...item, cleanedName: cleanName(item.name) }));
-
-const compareInitials = (ourName, semestreName) => {
-  const initialsOurName = getInitials(ourName);
-  const initialsSemestreName = getInitials(semestreName);
-  let points = 0;
-
-  initialsOurName.forEach((initial, i) => {
-    const index = initialsSemestreName.indexOf(initial);
-    if (index !== -1) {
-      points += index === i ? POINTS_BY_SAME_SPOT : POINTS_BY_OCCURRENCE;
-    }
-  });
-
-  return points;
-};
-
-const compareNames = (name1, name2) => {
-  const name1Words = cleanName(name1).toLowerCase().split(' ');
-  const name2Words = cleanName(name2).toLowerCase().split(' ');
-
-  const commonWords = name1Words.filter(word => name2Words.includes(word)).length;
-  const totalWords = new Set([...name1Words, ...name2Words]).size;
-  return commonWords / totalWords; // Jaccard index for word similarity
-};
-
-const matches = [];
-const unmatched = [];
-const prevMatches = new Set();
-
-oursNames.forEach(ourObj => {
-  let highestScore = 0;
-  let bestMatch = null;
-
-  listNuevoSemestre.forEach(semestreObj => {
-    const similarityScore = compareNames(ourObj.name, semestreObj.name);
-
-    // Utilize a more stringent threshold to prevent incorrect matches
-    if (similarityScore > highestScore && similarityScore > 0.5) {
-      highestScore = similarityScore;
-      bestMatch = { ...ourObj, matchedName: semestreObj.name };
-    }
-  });
-
-  if (bestMatch) {
-    matches.push(bestMatch);
-    prevMatches.add(bestMatch.semestre);
-  } else {
-    unmatched.push(ourObj);
-  }
-});
-
-fs.writeFile('./matcheds.json', JSON.stringify(matches, null, 2), err => {
-  if (err) console.error('Error writing file:', err);
-});
-
-fs.writeFile('./unmatched.json', JSON.stringify(unmatched, null, 2), err => {
-  if (err) console.error('Error writing file:', err);
-});
-
-console.log(`Matches: ${matches.length}`);
-console.log(`Unmatched: ${unmatched.length}`);
-console.timeEnd('Tiempo de ejecución');
-
-const proccesinExtract = async () => {
+const MAX_CONCURRENT_PAGES = 20; // Número máximo de páginas concurrentes.
+const processTeacherData = async () => {
   const browser = await chromium.launch({ headless: true });
-  const page = await browser.newPage();
-
-  const goToProcessing = async code => {
-    //Change code from LET-0120 to LET0120
-    const codeWithoutDash = code.replace('-', '');
-    const url = `https://www.nuevosemestre.com/programacion-docente/${codeWithoutDash}`;
-    await page.goto(url);
-  };
-
-  //click in a with href contain '/iniciar-sesion'
-  await page.click('a[href*="/iniciar-sesion"]');
-  await page.waitForNavigation({ waitUntil: 'networkidle', timeout: 60000 });
-
-  await page.fill('input[name="email"]', 'animeacademia09@gmail.com');
-  await page.fill('input[name="password"]', 'svNN4MZuqXZCcZU');
-  //Click in button type submit
-  await page.click('button[type="submit"]');
-  await page.waitForNavigation({ waitUntil: 'networkidle', timeout: 60000 });
-
-  await page.goto('https://www.nuevosemestre.com/calificar');
-
-  //Get all options in select input with name 'materia'
-  const options = await page.$$('select[name="name_teacher"] option');
-
-  //Get all values of options
-  const values = await Promise.all(
-    options.map(async option => {
-      return await option.evaluate(node => node.value);
-    })
+  const context = await browser.newContext();
+  const pages = await Promise.all(
+    Array.from({ length: MAX_CONCURRENT_PAGES }, () => context.newPage())
   );
 
-  //Get all text of options
-  const text = await Promise.all(
-    options.map(async option => {
-      return await option.evaluate(node => node.textContent);
-    })
-  );
+  const allConexion = [];
+  const notAvailable = [];
+  const queue = allSubjects.slice();
+  while (queue.length > 0) {
+    const tasks = queue
+      .splice(0, MAX_CONCURRENT_PAGES)
+      .map((subject, index) =>
+        processSingleSubjects(
+          pages[index % MAX_CONCURRENT_PAGES],
+          subject,
+          allConexion,
+          notAvailable
+        )
+      );
+    await Promise.all(tasks);
+  }
 
-  //Save option values and text in a object
-  const optionsValues = values.map((value, index) => {
-    return {
-      value: value,
-      name: text[index]
-    };
-  });
-
-  //Save in json file
-  await fs.writeFile('full_name_from_nuevosemestre.json', JSON.stringify(optionsValues, null, 2));
-
+  await saveData(allComments, notAvailable);
+  await context.close();
   await browser.close();
+  console.timeEnd('ProcessingTime');
 };
 
-proccesinExtract().catch(error => console.error(error));
+const processSingleSubjects = async (page, subject, allComments, notAvailable) => {
+  const maxRetries = 3; // Número máximo de reintentos.
+  let retries = 0;
+  while (retries < maxRetries) {
+    try {
+      //Transform string LET-0120 to LET0120
+      const subjectCode = subject.code.replace('-', '');
+      const url = `https://www.nuevosemestre.com/programacion-docente/${subjectCode}`;
+      //Go To nuevosemestre.com
+      await page.goto(url);
+      console.log('\x1b[32m%s\x1b[0m', `Processing ${subject.code}`);
+      //screenShot
+      await page.screenshot({ path: `screenshots/${subjectCode}.png` });
+      //Check if subjects are no opinions available
+      const noSubjectAviable = await page.$(
+        'img[src="https://media1.tenor.com/m/g0Tp5wjXrSgAAAAC/simpsons-batter.gif"]'
+      );
+      if (noSubjectAviable) {
+        console.log('No opinions available for:', subject.code);
+        notAvailable.push(subject);
+        return;
+      }
+
+      const SubjectData = {
+        id: subject.id,
+        name: subject.name,
+        code: subject.code,
+        codeFinal: subjectCode,
+        instructors: []
+      };
+
+      SubjectData.instructors = await extractComments(page);
+      // Handle pagination
+      const totalPages = await page.$$eval(
+        'button[type="button"][wire\\:click*="gotoPage"]',
+        buttons => buttons.length
+      );
+      console.log('Total pages:', totalPages, 'in ', SubjectData.code);
+
+      for (let pageNumber = 1; pageNumber <= totalPages; pageNumber++) {
+        await page.click(`button[wire\\:click="gotoPage(${pageNumber + 1}, 'page')"]`);
+        await page.waitForNavigation({ waitUntil: 'networkidle' });
+        const comments = await extractComments(page);
+        teacherData.comments.push(...comments);
+      }
+      allComments.push(teacherData);
+
+      return;
+    } catch (error) {
+      console.error('Error processing teacher:', teacher.matchedName, error);
+      // Manejar errores específicos o reintentar según sea necesario
+    }
+  }
+};
+
+const saveData = async (comments, notAvailable) => {
+  console.log('Saving data...');
+  await fs.writeFile('comments-uasd-teachers-v3.json', JSON.stringify(comments, null, 2));
+  await fs.writeFile(
+    'not-comments-available-teachers-v3.json',
+    JSON.stringify(notAvailable, null, 2)
+  );
+};
+
+const extractComments = async page => {
+  //screentshot
+  return page.$$eval('div[x-data="{ reply_box_is_visible: false }"]', comments =>
+    comments.map(comment => {
+      console.log(comment);
+      const username = comment.querySelector('p').textContent;
+      const period = comment.querySelector('p.text-sm').textContent;
+      const content = comment.querySelector('p.bg-neutral-300').textContent;
+      return { username, period, content };
+    })
+  );
+};
+
+processTeacherData().catch(console.error); // Inicia el proceso de procesamiento de datos de profesores.
+console.log('Fin de la comparación de nombres...');
+console.timeEnd('Tiempo de ejecución');
